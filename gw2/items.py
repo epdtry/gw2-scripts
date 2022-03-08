81480bc87151deee1bf846b44f4f66c75bcfb74f
epdtry <epdtry@epdtry.net>
2022-03-08 20:13:22 +0000

    download all items

--- gw2/items.py
+++ gw2/items.py
@@ -19,58 +19,48 @@ _DATA = None
 def _get_data():
     global _DATA
     if _DATA is None:
-        _refresh_if_needed()
-        _DATA = DataStorage(INDEX_FILE, DATA_FILE)
+        if gw2.build.need_refresh(BUILD_FILE):
+            _DATA = _refresh()
+        else:
+            _DATA = DataStorage(INDEX_FILE, DATA_FILE)
     return _DATA
 
-def _refresh_if_needed():
-    if gw2.build.need_refresh(BUILD_FILE):
-        # Clear the index and data files.
-        os.makedirs(ITEMS_DIR, exist_ok=True)
-        with open(INDEX_FILE, 'w'):
-            pass
-        with open(DATA_FILE, 'w'):
-            pass
-        with open(BUILD_FILE, 'w') as f:
-            f.write(str(gw2.build.current()))
+def _refresh():
+    # Clear the index and data files.
+    os.makedirs(ITEMS_DIR, exist_ok=True)
+    with open(INDEX_FILE, 'w'):
+        pass
+    with open(DATA_FILE, 'w'):
+        pass
+
+    data = DataStorage(INDEX_FILE, DATA_FILE)
+
+    all_ids = fetch('/v2/items')
+    all_ids.sort()
+
+    pos = 0
+    N = 100
+    for i in range(0, len(all_ids), N):
+        chunk = all_ids[i : i + N]
+        items = fetch('/v2/items?ids=' + ','.join(str(i) for i in chunk))
+        for i in items:
+            data.add(i['id'], i)
+
+    with open(BUILD_FILE, 'w') as f:
+        f.write(str(gw2.build.current()))
+
+    return data
 
 @functools.lru_cache(256)
 def get(item_id):
+    return _get_data().get(item_id)
+
+def iter_all():
     data = _get_data()
-    if not data.contains(item_id):
-        item = fetch('/v2/items?id=%d' % item_id)
-        data.add(item_id, item)
-        return item
-    else:
-        return data.get(item_id)
+    return (data.get(k) for k in data.keys())
 
 def get_multi(item_ids):
-    data = _get_data()
-
-    dct = {}
-    query_ids = []
-    for item_id in item_ids:
-        if data.contains(item_id):
-            dct[item_id] = data.get(item_id)
-        else:
-            query_ids.append(item_id)
-    query_ids = sorted(set(query_ids))
-
-    N = 100
-    for i in range(0, len(query_ids), N):
-        chunk = query_ids[i : i + N]
-        items = fetch('/v2/items?ids=' + ','.join(str(x) for x in chunk))
-        for item in items:
-            data.add(item['id'], item)
-            dct[item['id']] = item
-
-    out = []
-    for item_id in item_ids:
-        if item_id not in dct:
-            # Record that this item was not available from the API.
-            data.add(item_id, None)
-        out.append(dct.get(item_id))
-    return out
+    return [get(i) for i in item_ids]
 
 def name(item_id):
     return get(item_id)['name']
