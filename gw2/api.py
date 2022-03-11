a9224cfc01272f65557de09db160fc18d1614696
epdtry <epdtry@epdtry.net>
2022-03-11 04:27:56 +0000

    initial commit of bookkeeper.py

--- gw2/api.py
+++ gw2/api.py
@@ -8,6 +8,16 @@ API_BASE = 'https://api.guildwars2.com'
 API_KEY = None
 CACHE_DIR = None
 
+def _fetch_req(path):
+    headers = {}
+    if API_KEY is not None:
+        headers['Authorization'] = 'Bearer ' + API_KEY
+    url = API_BASE + path
+    print('fetch ' + url, file=sys.stderr)
+    r = requests.get(url, headers=headers)
+    r.raise_for_status()
+    return r
+
 def fetch(path, cache=False):
     if cache and CACHE_DIR is not None:
         os.makedirs(CACHE_DIR, exist_ok=True)
@@ -17,14 +27,7 @@ def fetch(path, cache=False):
             with open(cache_path) as f:
                 return json.load(f)
 
-    headers = {}
-    if API_KEY is not None:
-        headers['Authorization'] = 'Bearer ' + API_KEY
-    url = API_BASE + path
-    print('fetch ' + url, file=sys.stderr)
-    r = requests.get(url, headers=headers)
-    r.raise_for_status()
-    j = r.json()
+    j = _fetch_req(path).json()
 
     if cache and CACHE_DIR is not None:
         with open(cache_path, 'w') as f:
@@ -46,3 +49,18 @@ def fetch_with_retries(path, retry_count=3, seconds_between_retries=2, cache=Fal
         raise Exception('FetchWithRetryFailure')
 
     return response
+
+def fetch_paginated(path):
+    '''Fetch all pages of a paginated resource.  This is a generator that
+    yields each page in sequence, so the caller can break out of the loop to
+    stop fetching pages early.'''
+    r = _fetch_req(path)
+    yield r.json()
+
+    num_pages = int(r.headers.get('X-Page-Total', 0))
+    next_page = 1
+    while next_page < num_pages:
+        r = _fetch_req(path + '?page=%d' % next_page)
+        yield r.json()
+        num_pages = int(r.headers.get('X-Page-Total', 0))
+        next_page += 1
--- gw2/trading_post.py
+++ gw2/trading_post.py
@@ -5,7 +5,7 @@ import os
 import requests
 import time
 
-from gw2.api import fetch
+from gw2.api import fetch, fetch_paginated
 from gw2.constants import STORAGE_DIR
 import gw2.build
 from gw2.util import DataStorage
@@ -66,3 +66,81 @@ def get_prices_multi(item_ids):
             data.add(item_id, None)
         out.append(dct.get(item_id))
     return out
+
+
+def _update_history(kind):
+    '''Update the transaction history for `kind`, which must be either `'buys'`
+    or `'sells'`.  Returns the `DataStorage` object containing all the
+    transactions and a dict mapping item IDs to total quantity bought/sold.'''
+
+    os.makedirs(TRADING_POST_DIR, exist_ok=True)
+    index_file = os.path.join(TRADING_POST_DIR, 'history_%s_index.json' % kind)
+    data_file = os.path.join(TRADING_POST_DIR, 'history_%s_data.json' % kind)
+    data = DataStorage(index_file, data_file)
+
+    totals_file = os.path.join(TRADING_POST_DIR, 'history_%s_totals.json' % kind)
+    if os.path.exists(totals_file):
+        with open(totals_file) as f:
+            totals = defaultdict(int)
+            totals.update(dict(json.load(f)))
+    else:
+        totals = None
+
+    updated_totals = False
+    for page in fetch_paginated('/v2/commerce/transactions/history/%s' % kind):
+        done = False
+        for tx in page:
+            if data.contains(tx['id']):
+                done = True
+                break
+
+            data.add(tx['id'], tx)
+            if totals is not None:
+                totals[tx['item_id']] += tx['quantity']
+                updated_totals = True
+
+        if done:
+            break
+
+    if totals is None:
+        totals = defaultdict(int)
+        for tx in data.iter():
+            totals[tx['item_id']] += tx['quantity']
+        updated_totals = True
+
+    if updated_totals:
+        with open(totals_file, 'w') as f:
+            json.dump(list(totals.items()), f)
+
+    return data, totals
+
+@functools.lru_cache(2)
+def _get_history(kind):
+    return _update_history(kind)
+
+def total_bought():
+    return _get_history('buys')[1]
+
+def total_sold():
+    return _get_history('sells')[1]
+
+
+@functools.lru_cache(2)
+def _fetch_current(kind):
+    '''Fetch all current transactions for `kind`, which must be either `'buys'`
+    or `'sells'`.  Returns a dict mapping item IDs to total quantity across all
+    pending transactions.'''
+    transactions = []
+    counts = defaultdict(int)
+    for page in fetch_paginated('/v2/commerce/transactions/current/%s' % kind):
+        for tx in page:
+            counts[tx['item_id']] += tx['quantity']
+        transactions.extend(page)
+
+    return transactions, counts
+
+def pending_buys():
+    return _fetch_current('buys')
+
+def pending_sells():
+    return _fetch_current('sells')
