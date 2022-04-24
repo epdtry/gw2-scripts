4ccae44677511011653ddd5fd3af31ece4a559f3
jake <jake.wilke@gmail.com>
2022-04-24 16:11:15 -0500

    account_snapshot - gather data snapshot of account and write to data file

---
+++ DataSnapshot.py
@@ -0,0 +1,8 @@
+class DataSnapshot:
+    def __init__(self, char_name, inventory, materials, bank, char_core, magic_find):
+        self.char_name = char_name
+        self.inventory = inventory
+        self.materials = materials
+        self.bank = bank
+        self.char_core = char_core
+        self.magic_find = magic_find
---
+++ account_snapshot.py
@@ -0,0 +1,52 @@
+import json
+import os
+import sys
+import time
+import urllib.parse
+
+import gw2.api
+import DataSnapshot
+
+GW2_SCRIPTS_DIR = os.getcwd()
+GW2_DATA_DIR = os.path.join(os.path.abspath(os.path.join(GW2_SCRIPTS_DIR, os.pardir)), 'gw2-data')
+
+def get_inventory(char_name):
+    return gw2.api.fetch('/v2/characters/%s/inventory' %
+            urllib.parse.quote(char_name))
+
+def get_materials():
+    return gw2.api.fetch('/v2/account/materials')
+
+def get_bank():
+    return gw2.api.fetch('/v2/account/bank')
+
+def get_character_core(char_name):
+    return gw2.api.fetch('/v2/characters/%s/core' %
+            urllib.parse.quote(char_name))
+
+def main():
+    with open('api_key.txt') as f:
+        gw2.api.API_KEY = f.read().strip()
+    gw2.api.CACHE_DIR = 'cache'
+
+    char_name = sys.argv[1]
+    char_magic_find = sys.argv[2]
+
+    char_inventory = get_inventory(char_name)
+    char_materials = get_materials()
+    char_bank = get_bank()
+    char_core = get_character_core(char_name)
+
+    data = DataSnapshot.DataSnapshot(char_name, char_inventory, char_materials, char_bank, char_core, char_magic_find)
+
+    timestamp = time.strftime("%Y%m%d-%H%M%S")
+    data_file_name = char_name.replace(" ", "-") + '-' + timestamp + '.json'
+    print()
+    print('Data written to: ', data_file_name)
+    data_file = os.path.join(GW2_DATA_DIR, data_file_name)
+
+    with open(data_file, 'w') as f:
+        json.dump(data, f, default=vars)
+
+if __name__ == '__main__':
+    main()
