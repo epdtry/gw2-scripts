a32cd45dd0f37a474ae1f9728dcd5b66f0bd0ac9
jake <jake.wilke@gmail.com>
2022-05-01 16:01:40 -0500

    migrate snapshot diff to loot tool

--- loot_tool.py
+++ loot_tool.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 import json
 import os
 import sys
@@ -14,6 +15,7 @@ from loot_tool_models import *
 GW2_SCRIPTS_DIR = os.getcwd()
 GW2_DATA_DIR = os.path.join(os.path.abspath(os.path.join(GW2_SCRIPTS_DIR, os.pardir)), 'gw2-data')
 GW2_SNAPSHOT_DATA_DIR = os.path.join(GW2_DATA_DIR, "snapshots")
+GW2_DIFF_DATA_DIR = os.path.join(GW2_DATA_DIR, 'diffs')
 GW2_LOOT_BAG_FILE = os.path.join(GW2_DATA_DIR, 'loot_bags.txt')
 
 def get_inventory(char_name):
@@ -33,6 +35,94 @@ def get_character_core(char_name):
     return gw2.api.fetch('/v2/characters/%s/core' %
             urllib.parse.quote(char_name))
 
+def snapshot_from_file(file_path):
+    with open(file_path) as f:
+        text = f.read()
+        data_snapshot_dict = json.loads(text)
+    return DataSnapshot(**data_snapshot_dict)
+
+def is_valid_comparison(snapshot1, snapshot2):
+    if snapshot1.char_name != snapshot2.char_name:
+        return False
+    if snapshot1.magic_find != snapshot2.magic_find:
+        return False
+    if snapshot1.char_core['level'] != snapshot2.char_core['level']:
+        return False
+    if snapshot1.char_core['profession'] != snapshot2.char_core['profession']:
+        return False
+    return True
+
+def diff_magic(magic1, magic2):
+    return magic2 - magic1
+
+def diff_wallet(wallet1, wallet2):
+    wallet_diff = dict()
+
+    wallet1_dict = dict((currency['id'], currency['value']) for currency in wallet1)
+    wallet2_dict = dict((currency['id'], currency['value']) for currency in wallet2)
+
+    wallet1_keys = wallet1_dict.keys()
+    wallet2_keys = wallet2_dict.keys()
+    all_keys = set().union(*[wallet1_keys, wallet2_keys])
+
+    for currencyId in all_keys:
+        wallet1_value = wallet1_dict.get(currencyId, 0)
+        wallet2_value = wallet2_dict.get(currencyId, 0)
+        diff_value = wallet2_value - wallet1_value
+        if diff_value == 0:
+            continue
+        wallet_diff[currencyId] = diff_value
+
+    return wallet_diff
+
+def diff_items(inventory1, materials1, bank1, inventory2, materials2, bank2):
+    items_diff = defaultdict(int)
+
+    for bag in inventory2['bags']:
+        if bag is None:
+            continue
+        for item in bag['inventory']:
+            if item is None:
+                continue
+            items_diff[item['id']] += item['count']
+    
+    for item in materials2:
+        if item is None:
+            continue
+        items_diff[item['id']] += item['count']
+
+    for item in bank2:
+        if item is None:
+            continue
+        items_diff[item['id']] += item['count']
+
+    for bag in inventory1['bags']:
+        if bag is None:
+            continue
+        for item in bag['inventory']:
+            if item is None:
+                continue
+            items_diff[item['id']] -= item['count']
+    
+    for item in materials1:
+        if item is None:
+            continue
+        items_diff[item['id']] -= item['count']
+
+    for item in bank1:
+        if item is None:
+            continue
+        items_diff[item['id']] -= item['count']
+
+    final_items_diff = { item_id: value for item_id, value in items_diff.items() if value != 0 }
+    return final_items_diff
+
+def compute_diff(snapshot1, snapshot2):
+    wallet_diff = diff_wallet(snapshot1.wallet, snapshot2.wallet)
+    item_diff = diff_items(snapshot1.inventory, snapshot1.materials, snapshot1.bank, snapshot2.inventory, snapshot2.materials, snapshot2.bank)
+
+    return DataDiff(time.time(), snapshot1.char_name, wallet_diff, item_diff)
+
 def cmd_get_inventory(char_name):
     inventory = get_inventory(char_name)
     print(inventory)
@@ -42,11 +132,12 @@ def cmd_print_help():
     help_string = '''
     Command - Defition
     help - prints this documentation
-    inventory <char_name> - lists inventory of <char name>
+    inventory <char_name> - lists inventory of <char name>. Helper to see what info the snapshot command would fetch
     container_list - writes list of purchasable containers to ../gw2-data/loot_bags.txt
     snapshot <char_name> <magic_find> - creates a snapshot of char_name including inventory, materials, bank, wallet, and profession. This is all written to the ../gw2-data/snapshots/ directory
-    diff - todo
-    todomore
+    diff <file_path_to_snapshot_1> <file_path_to_snapshot_2> - takes a diff of two snapshots (snapshot2 - snapshot1) and writes the diff to ../gw2-data/snapshot/ directory
+    gen_loot_tables - prints the loot tables from all of the snapshots from ../gw2-data/snapshots/ directory
+    gen_worth_tables - prints the worth tables of the loot tables from all of the snapshots from ../gw2-data/snapshots/ directory
     '''
     print(help_string)
     return
@@ -101,6 +192,31 @@ def cmd_take_snapshot(char_name, char_magic_find):
     print('Data written to: ', data_file)
     return
 
+def cmd_take_diff(snapshot1_file, snapshot2_file):
+    snapshot1 = snapshot_from_file(snapshot1_file)
+    snapshot2 = snapshot_from_file(snapshot2_file)
+    
+    if not is_valid_comparison(snapshot1, snapshot2):
+        print('These files are not comparable. Check character name, level, magic find%....')
+        return
+    
+    diff_file_name = '-'.join(['diff', str(int(snapshot1.timestamp)), str(int(snapshot2.timestamp))]) + '.json'
+    diff_file = os.path.join(GW2_DIFF_DATA_DIR, diff_file_name)
+
+    diff_data = compute_diff(snapshot1, snapshot2)
+
+    with open(diff_file, 'w') as f:
+        json.dump(diff_data, f, default=vars)
+    print()
+    print('Data written to: ', diff_file)
+    return
+
+def cmd_gen_loot_tables():
+    return
+
+def cmd_gen_worth_tables():
+    return
+
 def main():
     ''' A tool with several commands to help with data collection of loot. 
     '''
@@ -122,10 +238,17 @@ def main():
     elif cmd == 'snapshot':
         char_name, char_magic_find = args
         cmd_take_snapshot(char_name, char_magic_find)
+    elif cmd == 'diff':
+        snapshot1_file, snapshot2_file = args
+        cmd_take_diff(snapshot1_file, snapshot2_file)
+    elif cmd == 'gen_loot_tables':
+        assert len(args) == 0
+        cmd_gen_loot_tables()
+    elif cmd == 'gen_worth_tables':
+        assert len(args) == 0
+        cmd_gen_worth_tables()
     else:
         raise ValueError('unknown command %r' % cmd)
-    
-
 
 if __name__ == '__main__':
     main()
--- loot_tool_models.py
+++ loot_tool_models.py
@@ -1,3 +1,5 @@
+import json
+
 class LootBag:
     def __init__(self, name, price, quantity, id):
         self.name = name
@@ -7,7 +9,6 @@ class LootBag:
  
     def __repr__(self):
         return json.dumps(self.__dict__)
-
 
 class DataSnapshot:
     def __init__(self, timestamp, char_name, inventory, materials, bank, wallet, char_core, magic_find):
@@ -19,3 +20,24 @@ class DataSnapshot:
         self.wallet = wallet
         self.char_core = char_core
         self.magic_find = magic_find
+
+class DataDiff:
+    def __init__(self, timestamp, char_name, wallet_diff, item_diff):
+        self.timestamp = timestamp
+        self.char_name = char_name
+        self.wallet_diff = wallet_diff
+        self.item_diff = item_diff
+
+class LootTable:
+    def __init__(self, timestamp, source_item_id, source_item_quantity, item_drop_info_list, wallet_drop_info_list):
+        self.timestamp = timestamp
+        self.source_item_id = source_item_id
+        self.source_item_quantity = source_item_quantity
+        self.item_drop_info_list = item_drop_info_list
+        self.wallet_drop_info_list = wallet_drop_info_list
+
+class DropInfo:
+    def __init__(self, id, quantity, drop_rate):
+        self.id = id
+        self.quantity = quantity
+        self.drop_rate = drop_rate
