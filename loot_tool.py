aacffa7a118be6085f418e1a826ed419f9d48dc8
jake <jake.wilke@gmail.com>
2022-05-01 15:38:06 -0500

    migrate account_snapshot to loot_tool

--- loot_tool.py
+++ loot_tool.py
@@ -7,29 +7,45 @@ import urllib.parse
 import gw2.api
 import gw2.items
 import gw2.recipes
-import gw2.mystic_forge
 import gw2.trading_post
 
-import DataSnapshot
 from loot_tool_models import *
 
 GW2_SCRIPTS_DIR = os.getcwd()
 GW2_DATA_DIR = os.path.join(os.path.abspath(os.path.join(GW2_SCRIPTS_DIR, os.pardir)), 'gw2-data')
 GW2_SNAPSHOT_DATA_DIR = os.path.join(GW2_DATA_DIR, "snapshots")
-LOOT_BAG_FILE = os.path.join(GW2_DATA_DIR, 'loot_bags.txt')
+GW2_LOOT_BAG_FILE = os.path.join(GW2_DATA_DIR, 'loot_bags.txt')
+
+def get_inventory(char_name):
+    return gw2.api.fetch('/v2/characters/%s/inventory' %
+            urllib.parse.quote(char_name))
+
+def get_materials():
+    return gw2.api.fetch('/v2/account/materials')
+
+def get_bank():
+    return gw2.api.fetch('/v2/account/bank')
+
+def get_wallet():
+    return gw2.api.fetch('/v2/account/wallet')
+
+def get_character_core(char_name):
+    return gw2.api.fetch('/v2/characters/%s/core' %
+            urllib.parse.quote(char_name))
 
 def cmd_get_inventory(char_name):
-    inventory = gw2.api.fetch('/v2/characters/%s/inventory' %
-            urllib.parse.quote(char_name))
+    inventory = get_inventory(char_name)
     print(inventory)
     return
 
 def cmd_print_help():
     help_string = '''
     Command - Defition
-    help - this
-    inventory <char name> - lists inventory of <char name>
-    container_list - writes list of purchasable containers to ../gw-data/loot_bags.txt
+    help - prints this documentation
+    inventory <char_name> - lists inventory of <char name>
+    container_list - writes list of purchasable containers to ../gw2-data/loot_bags.txt
+    snapshot <char_name> <magic_find> - creates a snapshot of char_name including inventory, materials, bank, wallet, and profession. This is all written to the ../gw2-data/snapshots/ directory
+    diff - todo
     todomore
     '''
     print(help_string)
@@ -62,11 +78,28 @@ def cmd_container_list():
 
     loot_bags.sort(key=lambda x: x.price)
     data = [lb for lb in loot_bags]
-    with open(LOOT_BAG_FILE, 'w') as f:
+    with open(GW2_LOOT_BAG_FILE, 'w') as f:
         json.dump(loot_bags, f, default=vars)
-    print('Data written to file: ', LOOT_BAG_FILE)
+    print('Data written to file: ', GW2_LOOT_BAG_FILE)
 
+def cmd_take_snapshot(char_name, char_magic_find):
+    char_inventory = get_inventory(char_name)
+    char_materials = get_materials()
+    char_bank = get_bank()
+    char_wallet = get_wallet()
+    char_core = get_character_core(char_name)
 
+    timestamp = time.time()
+    data = DataSnapshot(timestamp, char_name, char_inventory, char_materials, char_bank, char_wallet, char_core, char_magic_find)
+
+    data_file_name = str(int(timestamp)) + '.json'
+    data_file = os.path.join(GW2_SNAPSHOT_DATA_DIR, data_file_name)
+
+    with open(data_file, 'w') as f:
+        json.dump(data, f, default=vars)
+    print()
+    print('Data written to: ', data_file)
+    return
 
 def main():
     ''' A tool with several commands to help with data collection of loot. 
@@ -86,12 +119,9 @@ def main():
     elif cmd == 'container_list':
         assert len(args) == 0
         cmd_container_list()
-    elif cmd == 'goal':
-        count, name = args
-        cmd_goal(count, name)
-    elif cmd == 'craft_profit_buy':
-        assert len(args) == 0
-        cmd_craft_profit_buy()
+    elif cmd == 'snapshot':
+        char_name, char_magic_find = args
+        cmd_take_snapshot(char_name, char_magic_find)
     else:
         raise ValueError('unknown command %r' % cmd)
     
--- loot_tool_models.py
+++ loot_tool_models.py
@@ -7,3 +7,15 @@ class LootBag:
  
     def __repr__(self):
         return json.dumps(self.__dict__)
+
+
+class DataSnapshot:
+    def __init__(self, timestamp, char_name, inventory, materials, bank, wallet, char_core, magic_find):
+        self.timestamp = timestamp
+        self.char_name = char_name
+        self.inventory = inventory
+        self.materials = materials
+        self.bank = bank
+        self.wallet = wallet
+        self.char_core = char_core
+        self.magic_find = magic_find
