c86c20704524b7d9d4a982f340e069fe71bc39e5
epdtry <epdtry@epdtry.net>
2022-12-26 15:52:23 +0000

    obsidian.py: add jade sliver conversion

--- obsidian.py
+++ obsidian.py
@@ -43,14 +43,18 @@ def main():
     report([(25, 'Airship Part', 'currency'), (1050, 'Karma', 'currency')])
     report([(25, 'Lump of Aurillium', 'currency'), (1050, 'Karma', 'currency')])
     report([(25, 'Ley Line Crystal', 'currency'), (1050, 'Karma', 'currency')])
+
     report([(100, 'Unbound Magic', 'currency'), (96, 'Coin', 'currency')])
+    # Consume X map currency -> 100 unbound magic -> obsidian
     report([(3, 'Blood Ruby', 'item'), (96, 'Coin', 'currency')])
     report([(3, 'Petrified Wood', 'item'), (96, 'Coin', 'currency')])
     report([(5, 'Fresh Winterberry', 'item'), (96, 'Coin', 'currency')])
     report([(3, 'Jade Shard', 'item'), (96, 'Coin', 'currency')])
     report([(3, 'Fire Orchid Blossom', 'item'), (96, 'Coin', 'currency')])
     report([(5, 'Orrian Pearl', 'item'), (96, 'Coin', 'currency')])
+
     report([(100, 'Volatile Magic', 'currency'), (96, 'Coin', 'currency')])
+    # Consume X map currency -> 100 volatile -> obsidian
     report([(25, 'Kralkatite Ore', 'item'), (96, 'Coin', 'currency')])
     report([(5, 'Difluorite Crystal', 'item'), (96, 'Coin', 'currency')])
     report([(5, 'Inscribed Shard', 'item'), (96, 'Coin', 'currency')])
@@ -58,14 +62,21 @@ def main():
     report([(5, 'Branded Mass', 'item'), (96, 'Coin', 'currency')])
     report([(5, 'Mistborn Mote', 'item'), (96, 'Coin', 'currency')])
     report([(5, 'Difluorite Crystal', 'item'), (96, 'Coin', 'currency')])
+
+    # 75 eternal ice -> 10 mistborn mote -> 200 VM -> 2 obsidian
     report([(75, 'Eternal Ice Shard', 'item'), (2688, 'Karma', 'currency')],
             output_count=2)
+    # 10 blue shards -> 5 mistborn mote -> 100 VM -> obsidian
     report([(10, 'Blue Prophet Shard', 'currency'), (96, 'Coin', 'currency')])
+    # 10 green shards -> 10 blue shards, then as above
     report([(10, 'Green Prophet Shard', 'currency'), (96, 'Coin', 'currency')])
     report([
         (45, 'WvW Skirmish Claim Ticket', 'currency'),
         #(20, 'Memory of Battle', 'item'),
         ], output_count=15)
+    # 100 jade sliver -> 10 mistborn mote -> 200 VM -> 2 obsidian
+    report([(100, 'Jade Sliver', 'currency'),],
+            output_count=2)
 
 if __name__ == '__main__':
     main()
