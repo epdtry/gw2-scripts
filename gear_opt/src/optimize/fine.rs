7c5b53a76ea38cd7f0a419e0ca507f898625d898
epdtry <epdtry@epdtry.net>
2023-03-11 19:58:52 +0000

    gear_opt: refactor optimize::fine

--- gear_opt/src/optimize/fine.rs
+++ gear_opt/src/optimize/fine.rs
@@ -7,6 +7,102 @@ use crate::stats::Stats;
 use super::{AssertTotal, evaluate_config};
 
 
+struct SlotExt {
+    orig_idx: u8,
+    gear_slot: GearSlot,
+    quality: Quality,
+    stat_weight: f32,
+}
+
+#[derive(Clone, Debug)]
+struct Candidate {
+    slot_prefixes: Vec<u8>,
+}
+
+impl Candidate {
+    pub fn new(num_slots: usize) -> Candidate {
+        Candidate {
+            slot_prefixes: vec![0; num_slots],
+        }
+    }
+}
+
+struct Optimizer<'a, C: CharacterModel> {
+    ch: &'a C,
+    cfg: &'a C::Config,
+    slots: &'a [SlotExt],
+    prefixes: &'a [Prefix],
+    cur: Candidate,
+    best_m: f32,
+    best_candidate: Candidate,
+    tried: usize,
+}
+
+impl<'a, C: CharacterModel> Optimizer<'a, C> {
+    pub fn new(
+        ch: &'a C,
+        cfg: &'a C::Config,
+        slots: &'a [SlotExt],
+        prefixes: &'a [Prefix],
+    ) -> Optimizer<'a, C> {
+        Optimizer {
+            ch, cfg, slots, prefixes,
+            cur: Candidate::new(slots.len()),
+            best_m: 999999999.,
+            best_candidate: Candidate::new(slots.len()),
+            tried: 0,
+        }
+    }
+
+    fn evaluate_current(&mut self, gear: Stats) -> f32 {
+        self.tried += 1;
+        evaluate_config(self.ch, &gear, self.cfg)
+    }
+
+    fn go(&mut self, i: usize, gear: Stats) {
+        if i >= self.slots.len() {
+            let m = self.evaluate_current(gear);
+            if m < self.best_m {
+                self.best_m = m;
+                self.best_candidate = self.cur.clone();
+                eprintln!("metric = {}", m);
+                eprintln!("gear = {:?}", gear);
+                eprintln!();
+            }
+            return;
+        }
+
+        let slot = &self.slots[i];
+
+        for (j, prefix) in self.prefixes.iter().enumerate() {
+            let j = j as u8;
+
+            // Symmetry breaking: for runs of slots with identical weights, such as Ring1/Ring2 or
+            // Shoulders/Gloves/Boots, require prefixes to run in increasing order across the
+            // group.
+            if i > 0 &&
+                    slot.stat_weight == self.slots[i - 1].stat_weight &&
+                    j < self.cur.slot_prefixes[i - 1] {
+                continue;
+            }
+
+            self.cur.slot_prefixes[i] = j as u8;
+
+            if i < 2 {
+                eprintln!("{:?}", &self.cur.slot_prefixes[.. i + 1]);
+            }
+
+            let slot_stats = GEAR_SLOTS[slot.gear_slot].calc_stats(prefix, slot.quality)
+                .map(|_, x| x.round());
+            self.go(i + 1, gear + slot_stats);
+        }
+    }
+
+    pub fn optimize(&mut self) {
+        self.go(0, Stats::default());
+    }
+}
+
 pub fn optimize_fine<C: CharacterModel>(
     ch: &C,
     cfg: &C::Config,
@@ -19,87 +115,25 @@ pub fn optimize_fine<C: CharacterModel>(
     let mut slots = slots.iter().enumerate().map(|(i, &(slot, quality))| {
         let power = GEAR_SLOTS[slot].calc_stats(prefix_berserker, quality).power;
         let weight = power / prefix_berserker.formulas.power.factor;
-        (i as u8, slot, quality, weight)
+        SlotExt {
+            orig_idx: i as u8,
+            gear_slot: slot,
+            quality,
+            stat_weight: weight,
+        }
     }).collect::<Vec<_>>();
-    slots.sort_by_key(|&(_, _, _, w)| Reverse(AssertTotal(w)));
+    slots.sort_by_key(|s| Reverse(AssertTotal(s.stat_weight)));
 
     let prefixes = prefix_idxs.iter().map(|&i| PREFIXES[i]).collect::<Vec<_>>();
 
-    // The prefix chosen for each slot.  Each value is an index into `prefix_idxs`/`prefixes`.
-    let mut choices = vec![0; slots.len()];
-    let mut best = (999999999., vec![0; slots.len()]);
-    let mut tried = 0;
+    let mut opt = Optimizer::new(ch, cfg, &slots, &prefixes);
+    opt.optimize();
 
-    fn go<C: CharacterModel>(
-        ch: &C,
-        cfg: &C::Config,
-        slots: &[(u8, GearSlot, Quality, f32)],
-        prefixes: &[Prefix],
-        choices: &mut [u8],
-        best: &mut (f32, Vec<u8>),
-        tried: &mut usize,
-        i: usize,
-        gear: Stats,
-    ) {
-        if i >= slots.len() {
-            *tried += 1;
-            let m = evaluate_config(ch, &gear, &cfg);
-            let (ref mut best_m, ref mut best_choices) = *best;
-            if m < *best_m {
-                *best_m = m;
-                *best_choices = choices.to_owned();
-                eprintln!("metric = {}", m);
-                eprintln!("gear = {:?}", gear);
-                eprintln!();
-            }
-            return;
-        }
-
-        let (_orig_idx, slot, quality, weight) = slots[i];
-
-        for (j, prefix) in prefixes.iter().enumerate() {
-            let j = j as u8;
-
-            // Symmetry breaking: for runs of slots with identical weights, such as Ring1/Ring2 or
-            // Shoulders/Gloves/Boots, require prefixes to run in increasing order across the
-            // group.
-            if i > 0 && weight == slots[i - 1].3 && j < choices[i - 1] {
-                continue;
-            }
-
-            choices[i] = j as u8;
-
-            if i < 2 {
-                eprintln!("{:?}", &choices[.. i + 1]);
-            }
-
-            let slot_stats = GEAR_SLOTS[slot].calc_stats(prefix, quality)
-                .map(|_, x| x.round());
-            go(ch, cfg, slots, prefixes, choices, best, tried, i + 1, gear + slot_stats);
-        }
-    }
-
-    go(
-        ch,
-        cfg,
-        &slots,
-        &prefixes,
-        &mut choices,
-        &mut best,
-        &mut tried,
-        0,
-        Stats::default(),
-    );
-
-    let (ref best_m, ref best_choices) = best;
-    eprintln!("tried = {}", tried);
-    //eprintln!("slots = {:?}", slots);
-    //eprintln!("prefix_idxs = {:?}", prefix_idxs);
-    //eprintln!("best_choices = {:?}", best_choices);
-    eprintln!("best metric = {}", best_m);
+    eprintln!("tried = {}", opt.tried);
+    eprintln!("best metric = {}", opt.best_m);
     let mut out = vec![0; slots.len()];
-    for (&(orig_idx, _, _, _), choice) in slots.iter().zip(best_choices.iter()) {
-        out[orig_idx as usize] = prefix_idxs[*choice as usize];
+    for (slot, choice) in slots.iter().zip(opt.best_candidate.slot_prefixes.iter()) {
+        out[slot.orig_idx as usize] = prefix_idxs[*choice as usize];
     }
 
     out
