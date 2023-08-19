d9d7b64bcde9edebdf9198789c3f4751b8594325
epdtry <epdtry@epdtry.net>
2023-08-19 14:33:04 +0000

    cv_tp: find price on first row

--- cv_tp.py
+++ cv_tp.py
@@ -4,6 +4,16 @@ import numpy as np
 import cv2
 import time
 
+
+def vadd(xs, ys):
+    assert len(xs) == len(ys)
+    return tuple(x + y for x, y in zip(xs, ys))
+
+def vsub(xs, ys):
+    assert len(xs) == len(ys)
+    return tuple(x - y for x, y in zip(xs, ys))
+
+
 BASE_DIR = os.path.dirname(__file__)
 
 _ASSETS = {}
@@ -29,19 +39,31 @@ def found(pos, size):
             size = (shape[1], shape[0])
         _FOUND.append((pos, size))
 
-def find_template(gray_img, template_name, threshold = 0.85):
+def find_template(gray_img, template_name, threshold = 0.85, multi = False, base = None):
     template = asset(template_name, gray=True)
     res = cv2.matchTemplate(gray_img, template, cv2.TM_CCOEFF_NORMED)
     ys, xs = np.where(res >= threshold)
-    assert len(xs) == len(ys) and len(xs) <= 1, 'weird np.where result: %r, %r' % (xs, ys)
-    if len(xs) == 0:
-        return None
-    elif len(xs) == 1:
-        pos = (xs[0], ys[0])
-        found(pos, template)
-        return pos
+    assert len(xs) == len(ys), 'weird np.where result: %r, %r' % (xs, ys)
+    if multi:
+        ps = list(zip(xs, ys))
+        for p in ps:
+            if base is None:
+                found(p, template)
+            else:
+                found(vadd(p, base), template)
+        return ps
     else:
-        assert False, 'unreachable'
+        if len(xs) == 0:
+            return None
+        elif len(xs) == 1:
+            pos = (xs[0], ys[0])
+            if base is None:
+                found(pos, template)
+            else:
+                found(vadd(pos, base), template)
+            return pos
+        else:
+            raise ValueError('got %d positions, but expected at most 1' % len(xs))
 
 def check_template(gray_img, pos, template_name, threshold = 0.85):
     '''Compare the portion of `gray_img` at `pos` to the template image loaded
@@ -98,7 +120,6 @@ def detect_tp_tab(gray, win_pos):
     elif transactions:
         return 'transactions'
 
-
 _LAST_BLTC_POS = None
 def find_bltc(gray):
     global _LAST_BLTC_POS
@@ -109,6 +130,47 @@ def find_bltc(gray):
     bltc_pos = find_template(gray, 'bltc-header.png')
     _LAST_BLTC_POS = bltc_pos
     return bltc_pos
+
+PRICE_OFFSETS_BUY = [(830, 186), (830, 150), (830, 222)]
+PRICE_OFFSETS_SELL = [(893, 150), (893, 186)]
+
+def find_price_header(gray, tab, win_pos):
+    if tab == 'buy':
+        offsets = PRICE_OFFSETS_BUY
+    elif tab == 'sell':
+        offsets = PRICE_OFFSETS_SELL
+    else:
+        return None
+
+    for off in offsets:
+        pos = vadd(win_pos, off)
+        if check_template(gray, pos, 'tp-price.png'):
+            return pos
+    return None
+
+# Height of the rows in the buy/sell tabs
+ROW_HEIGHT = 61
+
+def find_first_price(gray, price_pos):
+    return find_first_price_with_template(gray, price_pos, 'coin-copper.png')
+
+def find_first_price_with_template(gray, price_pos, template_name):
+    template = asset(template_name)
+    x1 = 74
+    x0 = x1 - template.shape[1]
+    y0 = 20
+    y1 = y0 + ROW_HEIGHT + template.shape[0]
+    px, py = price_pos
+    region = gray[py + y0 : py + y1, px + x0 : px + x1]
+    offs = find_template(region, template_name, multi = True,
+            base = vadd(price_pos, (x0, y0)))
+    if len(offs) == 0:
+        return None
+    else:
+        # Return only the Y coordinate.  The X is a fixed offset.
+        return offs[0][1]
+
+
 
 FOUND_COLORS = [
         (0, 0, 255),
@@ -134,11 +196,18 @@ def process(img):
     if bltc_pos is not None:
         mode = detect_bltc_mode(gray, bltc_pos)
 
+        tab = None
         if mode == 'tp':
             tab = detect_tp_tab(gray, bltc_pos)
-        else:
-            tab = None
-        print(bltc_pos, mode, tab)
+
+        price_pos = None
+        if tab in ('buy', 'sell'):
+            price_pos = find_price_header(gray, tab, bltc_pos)
+
+        if price_pos is not None:
+            print('first row price y = ', find_first_price(gray, price_pos))
+
+        print(bltc_pos, mode, tab, price_pos)
 
     thumb_w = 300
     ratio = thumb_w / w
