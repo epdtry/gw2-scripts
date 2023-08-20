0eb2f70701989503dab73a0a6efae2e0b56c8db7
epdtry <epdtry@epdtry.net>
2023-08-20 19:05:18 +0000

    cv_tp: read prices

--- cv_tp.py
+++ cv_tp.py
@@ -31,6 +31,11 @@ def asset(name, gray=False):
     _ASSETS[key] = img
     return img
 
+def asset_size(name):
+    shape = asset(name).shape
+    return shape[1], shape[0]
+
+
 _FOUND = None
 def found(pos, size):
     if _FOUND is not None:
@@ -39,9 +44,29 @@ def found(pos, size):
             size = (shape[1], shape[0])
         _FOUND.append((pos, size))
 
-def find_template(gray_img, template_name, threshold = 0.85, multi = False, base = None):
+_TEXT = None
+def show_text(pos, s):
+    if _TEXT is not None:
+        _TEXT.append((pos, s))
+
+
+def find_template(gray_img, template_name, threshold = 0.85, multi = False,
+        best_match = False, base = None):
     template = asset(template_name, gray=True)
     res = cv2.matchTemplate(gray_img, template, cv2.TM_CCOEFF_NORMED)
+
+    if best_match:
+        index = np.argmax(res)
+        y, x = np.unravel_index(index, res.shape)
+        if res[y, x] < threshold:
+            return None
+        pos = (x, y)
+        if base is None:
+            found(pos, template)
+        else:
+            found(vadd(pos, base), template)
+        return pos
+
     ys, xs = np.where(res >= threshold)
     assert len(xs) == len(ys), 'weird np.where result: %r, %r' % (xs, ys)
     if multi:
@@ -78,6 +103,34 @@ def check_template(gray_img, pos, template_name, threshold = 0.85):
     if matches:
         found(pos, (sx, sy))
     return matches
+
+def best_template(gray_img, templates, threshold = 0.85, record_found = True):
+    '''Given several `(template_name, pos)` pairs, check each template against
+    the region of `gray_img` at `pos`, and return the index of the strongest
+    match.  If no match had a strength exceeding `threshold`, this function
+    returns `None`.
+    '''
+    best_index = None
+    best_strength = 0
+    for index, (template_name, (x, y)) in enumerate(templates):
+        template = asset(template_name, gray=True)
+        sy, sx = template.shape
+        region = gray_img[y : y + sy, x : x + sx]
+        res = cv2.matchTemplate(region, template, cv2.TM_CCOEFF_NORMED)
+        strength = res[0,0]
+        if strength > best_strength:
+            best_strength = strength
+            best_index = index
+
+    if best_strength < threshold:
+        return None
+
+    if record_found:
+        name, pos = templates[best_index]
+        found(pos, asset(name))
+
+    return best_index
+
 
 def detect_bltc_mode(gray, win_pos):
     win_x, win_y = win_pos
@@ -156,20 +209,69 @@ def find_first_price(gray, price_pos):
 
 def find_first_price_with_template(gray, price_pos, template_name):
     template = asset(template_name)
-    x1 = 74
+    x1 = 75
     x0 = x1 - template.shape[1]
     y0 = 20
-    y1 = y0 + ROW_HEIGHT + template.shape[0]
+    y1 = y0 + ROW_HEIGHT + template.shape[0] + 0
     px, py = price_pos
     region = gray[py + y0 : py + y1, px + x0 : px + x1]
-    offs = find_template(region, template_name, multi = True,
-            base = vadd(price_pos, (x0, y0)))
-    if len(offs) == 0:
+    base = vadd(price_pos, (x0, y0))
+    pos = find_template(region, template_name, best_match = True, base = base)
+    if pos is None:
         return None
-    else:
-        # Return only the Y coordinate.  The X is a fixed offset.
-        return offs[0][1]
+    return vadd(base, pos)
 
+COIN_TIERS = [
+        'coin-copper.png',
+        'coin-silver.png',
+        'coin-gold.png',
+        ]
+
+DIGIT_WIDTH = 8
+# Offset of digit templates above the top of the coin template.
+DIGIT_Y_OFFSET = 2
+
+def read_price(gray, coin_pos):
+    '''Given the position of the last coin icon, extract the full price.
+    Returns the total price in copper.'''
+    if coin_pos is None:
+        return None
+    price = 0
+    next_tier = 1
+    tier_mult = 1
+    digit_mult = 1
+    x, y = coin_pos
+    digits = []
+    value = 0
+    while True:
+        candidates = [('digit-%d.png' % i, (x - DIGIT_WIDTH, y - DIGIT_Y_OFFSET))
+                for i in range(10)]
+        if next_tier < len(COIN_TIERS):
+            coin = COIN_TIERS[next_tier]
+            coin_sx, coin_sy = asset_size(coin)
+            candidates.append((coin, (x - coin_sx, y)))
+
+        index = best_template(gray, candidates, record_found = False)
+        if index is None:
+            break
+
+        if index < 10:
+            digits.append(index)
+            x -= DIGIT_WIDTH
+            value += index * digit_mult
+            digit_mult *= 10
+        else:
+            digits.append(None)
+            x -= coin_sx
+            next_tier += 1
+            tier_mult *= 100
+            digit_mult = tier_mult
+
+    sx, sy = asset_size('coin-copper.png')
+    found((x, y), (coin_pos[0] + sx - x, sy))
+    show_text(vadd(coin_pos, (10 + sx, sy)), str(value))
+
+    return value
 
 
 FOUND_COLORS = [
@@ -184,9 +286,10 @@ FOUND_COLORS = [
         ]
 
 def process(img):
-    global _FOUND
+    global _FOUND, _TEXT
     h, w, depth = img.shape
     _FOUND = []
+    _TEXT = []
 
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
@@ -200,14 +303,24 @@ def process(img):
         if mode == 'tp':
             tab = detect_tp_tab(gray, bltc_pos)
 
-        price_pos = None
+        price_header_pos = None
         if tab in ('buy', 'sell'):
-            price_pos = find_price_header(gray, tab, bltc_pos)
+            price_header_pos = find_price_header(gray, tab, bltc_pos)
 
-        if price_pos is not None:
-            print('first row price y = ', find_first_price(gray, price_pos))
+        if price_header_pos is not None:
+            first_price_pos = find_first_price(gray, price_header_pos)
+            if first_price_pos is not None:
+                prices = []
+                for row in range(10):
+                    coin_pos = (first_price_pos[0], first_price_pos[1] + row * ROW_HEIGHT)
+                    if not check_template(gray, coin_pos, 'coin-copper.png', threshold = 0.95):
+                        break
+                    price = read_price(gray, coin_pos)
+                    prices.append(price)
+                print('prices', prices)
 
-        print(bltc_pos, mode, tab, price_pos)
+
+        #print(bltc_pos, mode, tab)
 
     thumb_w = 300
     ratio = thumb_w / w
@@ -221,7 +334,13 @@ def process(img):
         x1 = round((x + sx) * ratio)
         y1 = round((y + sy) * ratio)
         cv2.rectangle(thumb, (x0, y0), (x1, y1), color, 1)
-        pass
+
+    for (pos, s) in _TEXT:
+        x, y = pos
+        x = round(x * ratio)
+        y = round(y * ratio)
+        print('put %s at %d,%d' % (s, x, y))
+        cv2.putText(thumb, s, (x, y), cv2.FONT_HERSHEY_PLAIN, 0.6, (255, 0, 255))
 
     return thumb
 
