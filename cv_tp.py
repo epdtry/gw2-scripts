bff0b9c2450d5b551b176e7c67696a6775551185
epdtry <epdtry@epdtry.net>
2023-08-20 23:16:44 +0000

    cv_tp: read item names

--- cv_tp.py
+++ cv_tp.py
@@ -1,4 +1,5 @@
 import itertools
+import json
 import os
 import numpy as np
 import cv2
@@ -12,6 +13,9 @@ def vadd(xs, ys):
 def vsub(xs, ys):
     assert len(xs) == len(ys)
     return tuple(x - y for x, y in zip(xs, ys))
+
+def vmul(xs, c):
+    return tuple(x * c for x in xs)
 
 
 BASE_DIR = os.path.dirname(__file__)
@@ -204,18 +208,19 @@ def find_price_header(gray, tab, win_pos):
 # Height of the rows in the buy/sell tabs
 ROW_HEIGHT = 61
 
-def find_first_price(gray, price_pos):
-    return find_first_price_with_template(gray, price_pos, 'coin-copper.png')
+def find_first_price(gray, tab, price_pos):
+    return find_first_price_with_template(gray, tab, price_pos, 'coin-copper.png')
 
-def find_first_price_with_template(gray, price_pos, template_name):
+def find_first_price_with_template(gray, tab, price_pos, template_name):
     template = asset(template_name)
-    x1 = 75
+    x1 = 75 - (17 if tab == 'buy' else 0)
     x0 = x1 - template.shape[1]
     y0 = 20
     y1 = y0 + ROW_HEIGHT + template.shape[0] + 0
     px, py = price_pos
     region = gray[py + y0 : py + y1, px + x0 : px + x1]
     base = vadd(price_pos, (x0, y0))
+    found((px + x0, py + y0), (1, 1))
     pos = find_template(region, template_name, best_match = True, base = base)
     if pos is None:
         return None
@@ -273,6 +278,140 @@ def read_price(gray, coin_pos):
 
     return value
 
+NAME_OFFSET0 = (-570, -3)
+NAME_OFFSET1 = (-152, 10)
+NAME_HEIGHT = NAME_OFFSET1[1] - NAME_OFFSET0[1]
+# Offset of the start of the price column on the buy tab, relative to its
+# position on the sell and transaction tabs
+BUY_PRICE_OFFSET_X = 63 + 17
+
+def extract_name(image, tab, coin_pos):
+    x0, y0 = vadd(coin_pos, NAME_OFFSET0)
+    x1, y1 = vadd(coin_pos, NAME_OFFSET1)
+
+    if tab == 'buy':
+        # On the buy tab, the presence of the Favorite column shifts the price
+        # column to the left and reduces the width available for the name.
+        x0 += BUY_PRICE_OFFSET_X
+
+    found((x0, y0), (x1 - x0, y1 - y0))
+    return (image[y0:y1, x0:x1], (x0, y0))
+
+def trim_name(image):
+    assert len(image.shape) == 2
+    col_max = np.max(image, axis = 0)
+    col_min = np.min(image, axis = 0)
+    col_range = col_max - col_min
+    window_range = np.max(np.lib.stride_tricks.sliding_window_view(
+        col_range, window_shape = 7), axis = 1)
+    width = np.argmax(window_range < 30)
+    sy, sx = image.shape
+    return image[:, 0:width]
+
+
+def get_mtime(path):
+    if not os.path.exists(path):
+        return None
+    return os.stat(path).st_mtime
+
+INPUT_PATH = 'input.txt'
+_LAST_INPUT_MTIME = get_mtime(INPUT_PATH)
+_LAST_INPUT_STAT = time.time()
+
+def input_changed():
+    global _LAST_INPUT_MTIME, _LAST_INPUT_STAT
+    now = time.time()
+    if now < _LAST_INPUT_STAT + 1:
+        return False
+    mtime = get_mtime(INPUT_PATH)
+    if mtime != _LAST_INPUT_MTIME:
+        _LAST_INPUT_MTIME = mtime
+        _LAST_INPUT_STAT = now
+        return True
+    return False
+
+
+class NameCache:
+    def __init__(self, path):
+        os.makedirs(path, exist_ok = True)
+        self.path = path
+        # Cache contents.  Map from image width to a pair of (image data,
+        # name list).  To add an entry, we look up the image and names for its
+        # width, add a new row (consisting of the image data flattened out to
+        # 1px high) to the image, and add the name to the name list.  To do a
+        # lookup, we find the matching row and return the corresponding name.
+        self.dct = {}
+        # We store the first unknown image we see each frame.  The user can
+        # input a translation to add an entry to the cache.
+        self.unknown_image = None
+
+    def _load(self, width):
+        img_path = os.path.join(self.path, '%d.bin' % width)
+        names_path = os.path.join(self.path, '%d.json' % width)
+        if not os.path.exists(img_path) or not os.path.exists(names_path):
+            names = []
+            img = np.ndarray((0, width * NAME_HEIGHT), dtype = 'uint8')
+        else:
+            names = json.load(open(names_path))
+            count = len(names)
+            img = np.ndarray((count, width * NAME_HEIGHT), dtype = 'uint8',
+                    buffer = open(img_path, 'rb').read())
+        self.dct[width] = (img, names)
+        return (img, names)
+
+    def _save(self, width):
+        (img, names) = self.dct[width]
+        img_path = os.path.join(self.path, '%d.bin' % width)
+        names_path = os.path.join(self.path, '%d.json' % width)
+        count = len(names)
+        with open(img_path, 'wb') as f:
+            f.write(bytes(img.reshape(count * width * NAME_HEIGHT)))
+        with open(names_path, 'w') as f:
+            json.dump(names, f)
+
+    def _get_info(self, width):
+        info = self.dct.get(width)
+        if info is not None:
+            return info
+        info = self._load(width)
+        if info is not None:
+            self.dct[width] = info
+        return info
+
+    def lookup(self, text_image):
+        sy, sx = text_image.shape
+        width = sx
+        n = sy * sx
+        row = text_image.reshape((1, n))
+
+        img, names = self._get_info(width)
+
+        if len(names) == 0:
+            index = None
+            strength = 0
+        else:
+            res = cv2.matchTemplate(img, row, cv2.TM_CCOEFF_NORMED)
+            index = np.argmax(res)
+            strength = res[index, 0]
+
+        if strength < 0.95:
+            if self.unknown_image is None:
+                self.unknown_image = text_image
+            return None
+
+        return names[index]
+
+    def insert(self, text_image, text):
+        sy, sx = text_image.shape
+        width = sx
+        n = sy * sx
+        row = text_image.reshape((1, n))
+
+        img, names = self._get_info(width)
+        img = np.append(img, row, axis = 0)
+        names.append(text)
+        self.dct[width] = (img, names)
+        self._save(width)
 
 FOUND_COLORS = [
         (0, 0, 255),
@@ -285,11 +424,14 @@ FOUND_COLORS = [
         (255, 0, 255),
         ]
 
+NAME_CACHE = NameCache('storage/cv_tp_name_cache')
+
 def process(img):
     global _FOUND, _TEXT
     h, w, depth = img.shape
     _FOUND = []
     _TEXT = []
+    NAME_CACHE.unknown_image = None
 
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
@@ -308,19 +450,27 @@ def process(img):
             price_header_pos = find_price_header(gray, tab, bltc_pos)
 
         if price_header_pos is not None:
-            first_price_pos = find_first_price(gray, price_header_pos)
+            first_price_pos = find_first_price(gray, tab, price_header_pos)
+            print(first_price_pos)
             if first_price_pos is not None:
                 prices = []
                 for row in range(10):
                     coin_pos = (first_price_pos[0], first_price_pos[1] + row * ROW_HEIGHT)
                     if not check_template(gray, coin_pos, 'coin-copper.png', threshold = 0.95):
-                        break
+                        continue
                     price = read_price(gray, coin_pos)
                     prices.append(price)
-                print('prices', prices)
+
+                    (name_img, name_pos) = extract_name(gray, tab, coin_pos)
+                    name_img = trim_name(name_img)
+                    text = NAME_CACHE.lookup(name_img)
+                    if text is not None:
+                        sx, sy = asset_size('coin-copper.png')
+                        show_text(vadd(name_pos, (0, sy)), text)
+                    print(text, price)
 
 
-        #print(bltc_pos, mode, tab)
+        print(bltc_pos, mode, tab, price_header_pos)
 
     thumb_w = 300
     ratio = thumb_w / w
@@ -333,14 +483,28 @@ def process(img):
         y0 = round(y * ratio)
         x1 = round((x + sx) * ratio)
         y1 = round((y + sy) * ratio)
-        cv2.rectangle(thumb, (x0, y0), (x1, y1), color, 1)
+        cv2.rectangle(thumb, (x0, y0), (x1, y1), vmul(color, 0.5), 1)
 
     for (pos, s) in _TEXT:
         x, y = pos
         x = round(x * ratio)
         y = round(y * ratio)
-        print('put %s at %d,%d' % (s, x, y))
-        cv2.putText(thumb, s, (x, y), cv2.FONT_HERSHEY_PLAIN, 0.6, (255, 0, 255))
+        cv2.putText(thumb, s, (x, y), cv2.FONT_HERSHEY_PLAIN, 0.6,
+                (255, 0, 255), 1, cv2.LINE_AA)
+
+    # Copy unknown_image into place, if needed
+    if NAME_CACHE.unknown_image is not None:
+        unk_img = NAME_CACHE.unknown_image
+        sy, sx = unk_img.shape
+        sx = min(sx, thumb_w)
+        sy = min(sy, thumb_h)
+        thumb[0:sy, 0:sx, 2] = 255 - unk_img[0:sy, 0:sx]
+        thumb[0:sy, 0:sx, 1] = 0
+        thumb[0:sy, 0:sx, 0] = 0
+
+        if input_changed():
+            name = open(INPUT_PATH).read().strip()
+            NAME_CACHE.insert(NAME_CACHE.unknown_image, name)
 
     return thumb
 
